fba713c713633560a251d3388758b4f4172c68bd
NiGHT <86895149+NiGHT757@users.noreply.github.com>
2024-07-05 23:59:03 +0300

    v0.0.2

--- MapRestrictions.cs
+++ MapRestrictions.cs
@@ -13,7 +13,7 @@ public class MapRestrictions : BasePlugin
     public override string ModuleAuthor => "NiGHT";
     public override string ModuleName => "MapRestrictions";
     public override string ModuleDescription => "Restrict maps to certain players";
-    public override string ModuleVersion => "0.0.1";
+    public override string ModuleVersion => "0.0.2";
     
     public class MapData
     {
@@ -24,6 +24,8 @@ public class MapRestrictions : BasePlugin
     public class MapRestriction
     {
 	    public string Model { get; set; } = string.Empty;
+	    public bool UpdateOnPlayerConnect { get; set; } = false;
+	    public bool CountBots { get; set; } = false;
 	    public Dictionary<string, MapMessages> Messages { get; set; } = new();
 	    public Dictionary<string, MapArea> Areas { get; set; } = new();
     }
@@ -45,93 +47,11 @@ public class MapRestrictions : BasePlugin
 	    public string? Scale { get; set; }
     }
     
+    private string? _msg;
     private string? _path;
     private CCSGameRules? _gameRules;
     private MapData? _mapData;
     private readonly HashSet<CBaseModelEntity> _spawnedProps = new();
-    
-    private static CCSGameRules GetGameRules()
-    {
-	    return Utilities.FindAllEntitiesByDesignerName<CCSGameRulesProxy>("cs_gamerules").First().GameRules!;
-    }
-    
-    private static Vector StringToVector(string vector)
-	{
-	    var split = vector.Split(' ');
-	    return new Vector(float.Parse(split[0]), float.Parse(split[1]), float.Parse(split[2]));
-	}
-    
-	private static QAngle StringToQAngle(string vector)
-	{
-		var split = vector.Split(' ');
-		return new QAngle(float.Parse(split[0]), float.Parse(split[1]), float.Parse(split[2]));
-	}
-    
-    private void SpawnProp(string modelPath, Vector origin, QAngle angles, float scale = 0.0f)
-    {
-	    var prop = Utilities.CreateEntityByName<CBaseModelEntity>("prop_dynamic_override");
-
-	    if (prop == null) 
-		    return;
-
-	    prop.Collision.SolidType = SolidType_t.SOLID_VPHYSICS;
-	    prop.Teleport(origin, angles, new Vector(0, 0, 0));
-	    prop.DispatchSpawn();
-	    Server.NextFrame(() => prop.SetModel(modelPath));
-	    
-	    _spawnedProps.Add(prop);
-	    
-	    if(scale == 0.0f)
-		    return;
-	    
-	    var bodyComponent = prop.CBodyComponent;
-	    if (bodyComponent is not { SceneNode: not null })
-		    return;
-	    
-	    bodyComponent.SceneNode.GetSkeletonInstance().Scale = scale;
-    }
-    
-    private void SpawnProps()
-    {
-	    if(_mapData == null || _gameRules == null || _gameRules.WarmupPeriod)
-		    return;
-
-	    var players = Utilities.GetPlayers().Where(x => x.Connected == PlayerConnectedState.PlayerConnected)
-		    .ToList();
-	    var playersConnected = players.Count;
-	    
-	    var message = string.Empty;
-	    // find the message to send
-	    
-	    for (var i = 0; i < _mapData.Restrictions.Messages.Count; i++)
-	    {
-		    var mapMessage = _mapData.Restrictions.Messages.ElementAt(i).Value;
-		    if (playersConnected > mapMessage.MoreThan && (mapMessage.LessThan == 0 || playersConnected < mapMessage.LessThan))
-				message = mapMessage.Message;
-	    }
-	    
-	    if (!string.IsNullOrEmpty(message))
-	    {
-		    // count ct and t players
-		    var ctPlayers = players.Where(x => x is { Team: CsTeam.CounterTerrorist}).ToList().Count;
-		    var tPlayers = players.Where(x => x is { Team: CsTeam.Terrorist}).ToList().Count;
-		    
-		    Server.PrintToChatAll(Localizer["StartMessage"].Value.Replace("{tPlayers}", tPlayers.ToString()).Replace("{ctPlayers}", ctPlayers.ToString()).Replace("{message}", message));
-	    }
-	    
-	    // now let's spawn the props based on
-		var model = _mapData.Restrictions.Model;
-		for (var i = 0; i < _mapData.Restrictions.Areas.Count; i++)
-		{
-			var mapArea = _mapData.Restrictions.Areas.ElementAt(i).Value;
-			if (playersConnected > mapArea.MoreThan && (mapArea.LessThan == 0 || playersConnected < mapArea.LessThan))
-			{
-				if(mapArea is { Origin: not null, Angles: not null })
-					SpawnProp(model, StringToVector(mapArea.Origin), StringToQAngle(mapArea.Angles), string.IsNullOrEmpty(mapArea.Scale) ? 0.0f : float.Parse(mapArea.Scale));
-			}
-		}
-    }
-
     
     private void LoadConfig(string name)
     {
@@ -187,10 +107,7 @@ public class MapRestrictions : BasePlugin
 
     public override void Unload(bool hotReload)
     {
-	    foreach (var index in _spawnedProps.OfType<CBaseModelEntity>().Where(index => index.IsValid))
-		    index.Remove();
-	    
-	    _spawnedProps.Clear();
+	    ClearMapProps();
     }
 
     [GameEventHandler]
@@ -206,18 +123,28 @@ public class MapRestrictions : BasePlugin
 	    return HookResult.Continue;
     }
     
+    [GameEventHandler]
+    public HookResult OnPlayerConnect(EventPlayerConnect @event, GameEventInfo info)
+    {
+	    CheckCounter();
+
+	    return HookResult.Continue;
+    }
+
+    [GameEventHandler]
+    public HookResult OnPlayerDisconnect(EventPlayerDisconnect @event, GameEventInfo info)
+    {
+	    CheckCounter();
+	    
+	    return HookResult.Continue;
+    }
+
     [ConsoleCommand("maprestrictions_reload")]
     [RequiresPermissions("@css/root")]
     [CommandHelper(whoCanExecute: CommandUsage.CLIENT_AND_SERVER)]
     public void ReloadMapRestrictions(CCSPlayerController? caller, CommandInfo command)
 	{
-		foreach (var index in _spawnedProps.OfType<CBaseModelEntity>().Where(index => index.IsValid))
-		{
-			//Console.WriteLine($"{ModuleName} removed prop {index.Handle} + {index.DesignerName} + {index.Index}");
-			index.Remove();
-		}
-		
-		_spawnedProps.Clear();
+		ClearMapProps();
 		
 	    if (_path == null)
 		    return;
@@ -240,5 +167,120 @@ public class MapRestrictions : BasePlugin
 	    	caller.PrintToChat($"MapRestrictions - Reloaded map restrictions for {_mapData?.MapName}, found {newRestrictionsCount} restrictions and {newMessagesCount} messages");
 	    
 	    SpawnProps();
+	}
+    
+    private static CCSGameRules GetGameRules()
+    {
+	    return Utilities.FindAllEntitiesByDesignerName<CCSGameRulesProxy>("cs_gamerules").First().GameRules!;
+    }
+    
+    private static Vector StringToVector(string vector)
+	{
+	    var split = vector.Split(' ');
+	    return new Vector(float.Parse(split[0]), float.Parse(split[1]), float.Parse(split[2]));
+	}
+    
+	private static QAngle StringToQAngle(string vector)
+	{
+		var split = vector.Split(' ');
+		return new QAngle(float.Parse(split[0]), float.Parse(split[1]), float.Parse(split[2]));
+	}
+    
+    private void SpawnProp(string modelPath, Vector origin, QAngle angles, float scale = 0.0f)
+    {
+	    var prop = Utilities.CreateEntityByName<CBaseModelEntity>("prop_dynamic_override");
+
+	    if (prop == null) 
+		    return;
+
+	    prop.Collision.SolidType = SolidType_t.SOLID_VPHYSICS;
+	    prop.Teleport(origin, angles, new Vector(0, 0, 0));
+	    prop.DispatchSpawn();
+	    Server.NextFrame(() => prop.SetModel(modelPath));
+	    
+	    _spawnedProps.Add(prop);
+	    
+	    if(scale == 0.0f)
+		    return;
+	    
+	    var bodyComponent = prop.CBodyComponent;
+	    if (bodyComponent is not { SceneNode: not null })
+		    return;
+	    
+	    bodyComponent.SceneNode.GetSkeletonInstance().Scale = scale;
+    }
+    
+    private void SpawnProps()
+    {
+	    if(_mapData == null || _gameRules == null || _gameRules.WarmupPeriod)
+		    return;
+	    
+	    var players = Utilities.GetPlayers()
+		    .Where(x => x.Connected == PlayerConnectedState.PlayerConnected 
+		                && (!_mapData.Restrictions.CountBots || !x.IsBot))
+		    .ToList();
+
+	    var playersConnected = players.Count;
+	    
+	    var message = GetMapMessage(playersConnected);
+	    _msg = message;
+	    // find the message to send
+	    
+	    if (!string.IsNullOrEmpty(message))
+	    {
+		    // count ct and t players
+		    var ctPlayers = players.Where(x => x is { Team: CsTeam.CounterTerrorist}).ToList().Count;
+		    var tPlayers = players.Where(x => x is { Team: CsTeam.Terrorist}).ToList().Count;
+		    
+		    Server.PrintToChatAll(Localizer["StartMessage"].Value.Replace("{tPlayers}", tPlayers.ToString()).Replace("{ctPlayers}", ctPlayers.ToString()).Replace("{message}", message));
+	    }
+	    
+	    // now let's spawn the props based on
+		var model = _mapData.Restrictions.Model;
+		for (var i = 0; i < _mapData.Restrictions.Areas.Count; i++)
+		{
+			var mapArea = _mapData.Restrictions.Areas.ElementAt(i).Value;
+			if (playersConnected > mapArea.MoreThan && (mapArea.LessThan == 0 || playersConnected < mapArea.LessThan))
+			{
+				if(mapArea is { Origin: not null, Angles: not null })
+					SpawnProp(model, StringToVector(mapArea.Origin), StringToQAngle(mapArea.Angles), string.IsNullOrEmpty(mapArea.Scale) ? 0.0f : float.Parse(mapArea.Scale));
+			}
+		}
+    }
+    
+	private void CheckCounter()
+	{
+		if(_mapData == null || !_mapData.Restrictions.UpdateOnPlayerConnect || _msg == GetMapMessage(Utilities.GetPlayers().Where(x => x.Connected == PlayerConnectedState.PlayerConnected 
+			               && (!_mapData.Restrictions.CountBots || !x.IsBot)).ToList().Count))
+			return;
+
+		ClearMapProps();
+		SpawnProps();
+	}
+
+	private void ClearMapProps()
+	{
+		foreach (var index in _spawnedProps.OfType<CBaseModelEntity>().Where(index => index.IsValid))
+			index.Remove();
+		
+		_spawnedProps.Clear();
+	}
+	
+	private string GetMapMessage(int playersConnected)
+	{
+		if(_mapData == null)
+			return string.Empty;
+		
+		var message = String.Empty;
+		for (var i = 0; i < _mapData.Restrictions.Messages.Count; i++)
+		{
+			var mapMessage = _mapData.Restrictions.Messages.ElementAt(i).Value;
+			if (playersConnected > mapMessage.MoreThan && (mapMessage.LessThan == 0 || playersConnected < mapMessage.LessThan))
+			{
+				message = mapMessage.Message;
+			}
+		}
+
+		return message ?? String.Empty;
 	}
 }
